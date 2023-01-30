10d82fcb4fcb3e954694a7c7f3a90c1838925c1a
Mihai-Alexandru Tanasa <talexmihai@yahoo.com>
2023-01-30 21:15:20 +0200

    Solver Jar compilation by adding the Launcher class *use the main function within the Launcher class to compile the jar*

---
+++ src/main/java/application/Launcher.java
@@ -0,0 +1,8 @@
+package application;
+
+//This is here just to help with compilation to a jar file
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
